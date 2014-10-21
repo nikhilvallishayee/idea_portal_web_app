91acae4880ade868666fff726edd70d80ac5a487
PatternBot <awesome@patternbot.com>
2014-10-21 14:47:03 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeaPortalWebApp::Application.config.secret_token = 'c917212e1c98baa1f63cd2306e43a0ce63cbd73feb771b47483b97263173b55e0e9dc2f50584978418c2edbf854b6819ebecc6bd9202dca535c27d73e3c25d45'
