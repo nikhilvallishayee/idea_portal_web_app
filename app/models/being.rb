2989e4309be90bfbf4d75062b040b1837ed8e771
sonalbhanawat <sonal@gkmit.co>
2014-11-13 16:19:59 +0530

    user to being migration done

--- app/models/being.rb
+++ app/models/being.rb
@@ -5,12 +5,12 @@ include Clearance::User
  
   has_many :blogs
 
-  #has_attached_file :photo,:styles => { :large => "100x100>" , :medium => "50x50>", :small => "20x20>" }
+  has_attached_file :photo,:styles => { :large => "100x100>" , :medium => "50x50>", :small => "20x20>" }
   	#write code to store images in app/assets/images/beings/
-  #validates_presence_of :email, :name, :short_description
-  #validates_attachment_presence :photo
-  #validates_attachment_size :photo, :less_than => 5.megabytes
-  #validates_attachment_content_type :photo, :content_type => ['image/jpeg','image/jpg', 'image/png']
+  validates_presence_of :email, :name, :short_description
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg','image/jpg', 'image/png']
 
    def photo_url
         photo.url(:original)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141113101910) do
+ActiveRecord::Schema.define(:version => 20141113103356) do
 
   create_table "beings", :force => true do |t|
     t.string   "name"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(:version => 20141113101910) do
     t.string   "is_team_member"
     t.integer  "user_id"
     t.string   "password"
+    t.string   "encrypted_password"
+    t.string   "remember_token"
   end
 
   create_table "blogs", :force => true do |t|
@@ -40,17 +42,5 @@ ActiveRecord::Schema.define(:version => 20141113101910) do
     t.datetime "updated_at", :null => false
     t.integer  "being_id"
   end
-
-  create_table "users", :force => true do |t|
-    t.datetime "created_at",                        :null => false
-    t.datetime "updated_at",                        :null => false
-    t.string   "email",                             :null => false
-    t.string   "encrypted_password", :limit => 128, :null => false
-    t.string   "confirmation_token", :limit => 128
-    t.string   "remember_token",     :limit => 128, :null => false
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email"
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
