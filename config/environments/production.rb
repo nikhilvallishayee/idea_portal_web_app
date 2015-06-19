3423aecf28c4bcfedd44426319713f474dc7bbcf
sonal bhanawat <sonalbhanawat@yahoo.com>
2015-06-19 14:23:38 +0530

    changes in production .rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ IdeaPortalWebApp::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
 end
